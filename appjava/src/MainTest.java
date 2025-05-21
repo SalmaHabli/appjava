3a59fc44bd16bd5508e1aefc11f41f5f8cd892a7
ouma-chayb <omaymashayb@gmail.com>
2025-05-21 19:39:25 +0100

    Create MainTest.java

---
+++ appjava/src/MainTest.java
@@ -0,0 +1,8 @@
+public class MainTest {
+    public static void main(String[] args) throws Exception {
+        // Test basique pour exécuter Main.main()
+        System.out.println("Test démarré");
+        Main.main(new String[]{}); // Appelle la méthode main()
+        System.out.println("Test terminé");
+    }
+}
