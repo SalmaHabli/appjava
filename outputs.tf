69634ed359fdb99e292787cc57c2c37acd589962
SalmaHabli <habli.sa02@gmail.com>
2025-05-21 11:01:21 +0100

    Add files via upload

---
+++ outputs.tf
@@ -0,0 +1,16 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public_subnet[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private_subnet[*].id
+}
+
+
+output "nat_instance_id" {
+  value = aws_instance.nat_instance.id
+}
